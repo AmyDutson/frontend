8e59d24816f7ce1918108ed967a1011302f98165
Amy Fox <amyfox723@gmail.com>
2024-03-08 12:45:49 -0700

    broke cards into columns

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import yourData from './CollegeBasketballTeams.json';
 
-//header about site
+//header introducing site
 function Header() {
   return (
     <div>
       <h1>NCAA Basketball Info</h1>
+      <h2>Be Ready for March Madness!</h2>
     </div>
   );
 }
@@ -19,12 +20,20 @@ interface Info {
   state: string;
 }
 
+// team card listing school name, mascot, and location for each school
 class School extends React.Component<Info> {
   render() {
     const oneSchool = this.props;
 
     return (
-      <div style={{ border: '1px solid #000', padding: '10px' }}>
+      <div
+        style={{
+          border: '1px solid #000',
+          padding: '10px',
+          marginBottom: '10px',
+          width: '22.5%',
+        }}
+      >
         <h2>{oneSchool.school}</h2>
         <h3>Mascot: {oneSchool.name}</h3>
         <h3>
@@ -37,7 +46,13 @@ class School extends React.Component<Info> {
 
 function SchoolInfo() {
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+      }}
+    >
       {yourData.teams.map((schoolNum, index) => (
         <School key={index} {...schoolNum} />
       ))}
@@ -45,6 +60,7 @@ function SchoolInfo() {
   );
 }
 
+// list of all teams on the list
 class SchoolNames extends React.Component<Info> {
   render() {
     const oneSchool = this.props;
